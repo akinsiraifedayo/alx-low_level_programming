0fad70933ffd158171420b20f9420ffa546c7149
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-09 01:20:12 +0200

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@
 
 int main(void)
 {
-		write(0, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 100);
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 100);
 		return (1);
 }
