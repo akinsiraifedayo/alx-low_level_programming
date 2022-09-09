3f10294196aeda26b2117d7f06ce0b88962285a5
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-10 01:17:02 +0200

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	if (z > 5)
 		printf("%d and is greater than 5\n", z);
 	else if (z == 0)
-		printf("%d is 0\n", z);
+		printf("%d and is 0\n", z);
 	else if (z < 6 && z != 0)
 		printf("%d and is less than 6 and not 0\n", z);
 
