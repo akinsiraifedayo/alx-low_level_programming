2733963e968c4433403e8751cba7a9c907f83065
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-10 03:37:56 +0200

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,19 +2,26 @@
 /**
  *main - Where all the code runs
  *
- *Description: This program prints a-z of base 16
+ *Description: This program prints all base 16 numbers
  *
  *Return: returns 0
  */
 int main(void)
 {
-	char aStart = 0x61;
-	char bEnd = 0x7A;
+	char aStart = 0x60;
+	char bEnd = 0x71;
+	char cStart = 0x41;
+	char dEnd = 0x046;
 
 	while (aStart <= bEnd)
 	{
 		putchar (aStart);
 		aStart++;
+	}
+	while (cStart <= dEnd)
+	{
+		putchar (cStart);
+		cStart++;
 	}
 	putchar (10);
 	return (0);
