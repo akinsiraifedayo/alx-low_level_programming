c5371013bb84b6cde950d1ba5d33b39bbc1100ec
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-10 02:13:11 +0200

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ *main - Where all the code runs
+ *
+ *Description: This program prints A-Z
+ *
+ *Return: returns 0
+ */
+int main(void)
+{
+	char aStart = 97;
+	char bEnd = 122;
+	char Astart = 65;
+	char Bstart = 90;
+
+	while (aStart <= bEnd)
+	{
+		putchar (aStart);
+		aStart++;
+	}
+	while (Astart <= Bstart)
+	{
+		putchar (Astart);
+		Astart++;
+	}
+	putchar (10);
+	return (0);
+}
