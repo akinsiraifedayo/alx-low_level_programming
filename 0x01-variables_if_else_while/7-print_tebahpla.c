48499eae485bb55737de19c6940c9b7b28e52e52
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-10 03:15:55 +0200

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ *main - Where all the code runs
+ *
+ *Description: This program prints A-Z
+ *
+ *Return: returns 0
+ */
+int main(void)
+{
+	char aStart = 122;
+	char bEnd = 97;
+
+	while (aStart >= bEnd)
+	{
+		putchar (aStart);
+		aStart--;
+	}
+	putchar (10);
+	return (0);
+}
