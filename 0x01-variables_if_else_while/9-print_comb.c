be422e2bdc0bdeb9b2b2e9d22caa30f026e6dcdf
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-10 03:52:35 +0200

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *main - Where all the code runs
+ *
+ *Description: This program prints all numbers 0-9 seperted by commas
+ *
+ *Return: returns 0
+ */
+int main(void)
+{
+	char i;
+	char comma = 44;
+
+	for (i = 48; i < 57; i++)
+	{
+		putchar(i 44 32);
+	}
+	putchar (10);
+	return (0);
+}
