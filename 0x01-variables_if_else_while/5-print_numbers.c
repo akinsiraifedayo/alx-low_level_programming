975f14af90478221d5ebfddf5c9cec17b1452d13
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-10 02:46:13 +0200

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *main - Where all the code runs
+ *
+ *Description: This program prints A-Z
+ *
+ *Return: returns 0
+ */
+int main(void)
+{
+	char i = 0;
+
+	while (i >= 0, i < 10, i++)
+	{
+		putchar (i);
+	}
+	putchar (10);
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,5 @@
 1-last_digit.c check last digit of a number and prints it
 3-print_alphabets.c prints using putchar
 3-print_alphabets.c prints a-z and A-Z
+4-print_alphabt.c print all alphabets except q and e
+4-print_alphabt.c print all alphabets except q and e ls
