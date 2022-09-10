96b3133bb5b708ce37d7e64291acb0965c9300f7
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-10 04:19:55 +0200

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (i = 48; i <= 57; i++)
 	{
+		putchar(i);
 		if (i < 57)
 		{
-			putchar(i);
 			putchar(44);
+			putchar(32);	
 		}
-		putchar(32);
 	}
 	putchar (10);
 	return (0);
