0492ef80c2c5c43f21c9d255b5141bd61dba466d
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-13 20:19:52 +0200

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
- *_islower - function that checks lower case
- *
- *Description:
- *
- */
+ * * _islower - Shows 1 if the input is a
+ * * lowercase character. Another cases, shows 0
+ * *
+ * * @c: The character in ASCII code
+ * *
+ * * Return: 1 for lowercase. 0 for the rest.
+ * */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+		if (c >= 97 && c <= 122)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+		_putchar('\n');
 }
