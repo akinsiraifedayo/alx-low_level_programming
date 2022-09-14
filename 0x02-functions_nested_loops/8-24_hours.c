4f52746b29c9240e0a646ea96094825b0fad5bef
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-14 13:44:58 +0200

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *jack_bauer - function that prints time
+ *
+ *Description: prints time
+ */
+void jack_bauer(void)
+{
+	int startHour = 0;
+	int endHour = 24;
+
+	while (startHour <= endHour)
+	{
+		int startMin = 0;
+		int endMin = 59;
+
+		while (startMin <=  endMin)
+		{
+			printf("%02d:%02d\n", startHour, startMin);
+			startMin++;
+		}
+		startHour++;
+	}
+
+}
