f28e12417124ab3fa39ba7ab053d00b58ec649d2
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-14 12:42:59 +0200

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *print_last_digit - prints last digit
+ *
+ * @c - the integer we are checking
+ *
+ *Description: This function prints last digit of a number
+ *Return: returns last digit
+ */
+int print_last_digit(int c)
+{
+	int lastDigit;
+
+	lastDigit = (c % 100);
+	return (c);
+}
