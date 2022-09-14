5602065658970af3ae1e32a6d8673141071e609e
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-14 19:28:28 +0200

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *times_table - function that prints time
+ *
+ *Description: prints time
+ */
+void times_table(void)
+{
+	int startVert = 0;
+	int endVert = 9;
+
+	while (startVert <= endVert)
+	{
+		int startHoriz = 0;
+		int endHoriz = 9;
+
+		while (startHoriz <=  endHoriz)
+		{
+			int mResult = (startVert * startHoriz);
+			printf("%d", mResult);
+			if (startHoriz < endHoriz)
+				printf(", ");
+			startHoriz++;
+		}
+		startVert++;
+	}
+
+}
