51aab310fe46e8abf11cb0c93c559e5d0572efb8
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-14 12:24:21 +0200

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"i
+#include <math.h>
+/**
+ *int _abs - function that checks lower case
+ *
+ * @c: is the input of function lower
+ *Description: checks lowercase
+ *Return: either 1 or 0
+ */
+int _abs(int c)
+{
+	int preAbs;
+	int newAbs;
+
+	preAbs = (c*c);
+	newAbs = sqrt(preAbs);
+	printf ("%d", newAbs);
+}
