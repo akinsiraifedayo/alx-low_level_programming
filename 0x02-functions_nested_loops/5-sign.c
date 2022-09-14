9906a9ee5329ca36a79de6995e3fe0167bdbfac2
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-14 10:45:42 +0200

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,18 +11,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
 		return (1);
+		printf("+");
 	}
 	else if (n ==0)
 	{
-		printf("0");
 		return (0);
+		printf("0");
 	}
 	else if (n < 0)
 	{
-		printf("-");
 		return(-1);
+		printf("-");
 	}
 	else
 		return(0);
