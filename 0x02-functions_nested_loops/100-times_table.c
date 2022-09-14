a2bb9e4b023233013815d9dba8e38d100939952b
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-15 01:21:34 +0200

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,12 @@
 #include "stdio.h"
 #include "main.h"
 /**
- * * print_times_table - Prints the n times table
- * *
- * * @n: number times table (0 < n <= 15)
- * *
- * * Return: no return
- * */
+ * print_times_table - Prints the n times table
+ *
+ * @n: number times table (0 < n <= 15)
+ *
+ * Return: no return
+ **/
 void print_times_table(int n)
 {
 		int a;
@@ -16,34 +16,33 @@ void print_times_table(int n)
 
 		if (n >= 0 && n <= 15)
 		{
-			for (a = 0; a <= n; a++)						{
-				_putchar(48);
-												for (b = 1; b <= n; b++)
-												{
-													multResult = a * b;
-													_putchar(44);
-													_putchar(32);
-													if (multResult <= 9)
-													{
-														_putchar(32);
-														_putchar(32);
-																																	_putchar(multResult + 48);
-																																			}
-																									else if (multResult <= 99)
-																												{
-																															_putchar(32);
-																																	_putchar((multResult / 10) + 48);
-																																			_putchar((multResult % 10) + 48);
-																																					}
-																											else
-																														{
-																																	_putchar(((multResult / 100) % 10) + 48);
-																																			_putchar(((multResult / 10) % 10) + 48);
-																																					_putchar((multResult % 10) + 48);
-																																							}
-																												}
-													_putchar('\n');
-														}
-								}
+			for (a = 0; a <= n; a++)
+			{
+				_putchar(48);								for (b = 1; b <= n; b++)
+				{
+					multResult = a * b;
+					_putchar(44);
+					_putchar(32);
+					if (multResult <= 9)
+					{
+						_putchar(32);
+						_putchar(32);
+						_putchar(multResult + 48);
+					}
+					else if (multResult <= 99)
+					{
+						_putchar(32);
+						_putchar((multResult / 10) + 48);
+						_putchar((multResult % 10) + 48);
+					}
+					else
+					{
+						_putchar(((multResult / 100) % 10) + 48);
+						_putchar(((multResult / 10) % 10) + 48);
+						_putchar((multResult % 10) + 48);
+					}
+					}
+				_putchar('\n');
+			}
+		}
 }
-
