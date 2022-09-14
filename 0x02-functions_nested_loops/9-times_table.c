b5edc64f38f510a73a0693fc69f28fff81b5ce9a
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-14 20:20:04 +0200

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				printf("%d", mResult);
 			else
 			{
-			if (startHoriz < endHoriz)
+			if (startHoriz <= endHoriz)
 				if (mResult <= 9)
 					printf(",  %d", mResult);
 				else
