d0e5636df88606b5ae6aff1b6357f287418290f8
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-15 01:50:33 +0200

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - naturan numbers printer
+ *
+ *Description: This function prints common multiples of 3 and 5
+ *
+ */
+void main(void)
+{
+	int z = 1024;
+	int a = 0;
+
+	while ( a < z)
+	{
+		while ((a % 3 == 0 && a % 5 == 0) && (a =! 1005))
+		{
+			printf("%d ,");
+			a++;
+		}
+		printf("%d\n");
+
+	}
+
+}
