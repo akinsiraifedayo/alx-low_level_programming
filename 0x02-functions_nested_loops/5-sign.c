92234bb6483fcde4c9ac5b2411d60a071ad8535d
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-14 10:24:52 +0200

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_print_sign - function that prints sign of a number
+ *
+ * @n: is the input of function
+ *Description: checks for sign
+ *Return: either 1 or 0 or -1
+ */
+int _isalpha(int n)
+{
+	if (n > 0)
+		printf("+");
+		return (1);
+	else if (n ==0)
+		printf("0");
+		return (0);
+	else if (n < 0)
+		printf("-");
+		return(-1);
+}
