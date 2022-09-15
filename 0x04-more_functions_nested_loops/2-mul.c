e441862ac3e2f28f70eaadf7a8c9f3031063f563
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-15 18:16:32 +0200

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - function that multiplies two integers
+* @a: First int
+* @b: Second int
+* Return: mult
+*/
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+return (mult);
+}
