448b1e008729f546d742fd5ee8d098e6478fd911
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-15 04:01:59 +0200

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,19 +8,19 @@
  */
 int main(void)
 {
-	double second = 2;
-	double current = 3;
-	double sum = 0;
-	double myResult = 6;
-	double modCheck;
+	unsigned int second = 2;
+	unsigned int current = 3;
+	unsigned int sum = 0;
+	unsigned int myResult = 6;
+	unsigned int modCheck;
 
 	while (sum < 4000000)
 	{
 		sum = (current + second);
 		second = current;
 		current = sum;
-		modCheck = sum - (sum * (sum / 2));
-		if ((sum < 4000000) && (modCheck < 0.00001))
+		modCheck = (sum % 2);
+		if ((sum < 4000000) && (modCheck ==0))
 			myResult = (myResult + sum);
 	}
 	printf("%.0f\n", myResult);
