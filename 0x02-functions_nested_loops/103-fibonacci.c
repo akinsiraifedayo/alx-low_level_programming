b7443cafa0884796e74baeafea69d360e3bf1694
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-15 03:30:48 +0200

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,14 +12,14 @@ int main(void)
 	double current = 3;
 	double sum = 0;
 	double myResult = 6;
-	double modCheck = 2;
 
 	while (sum < 4000000)
 	{
 		sum = (current + second);
 		second = current;
 		current = sum;
-		if ((sum < 4000000) && (std::fmod(sum,modCheck)) < 1)
+		double modCheck = remainder(sum, 2);
+		if ((sum < 4000000) && modCheck < 1)
 			myResult = (myResult + sum);
 	}
 	printf("%.0f\n", myResult);
