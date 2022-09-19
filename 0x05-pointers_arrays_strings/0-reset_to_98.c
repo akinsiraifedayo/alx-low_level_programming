2d9a08053a42a9d7ec0425f22ccbf8c644f406e1
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-19 19:04:54 +0200

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-* reset_to_98 - function that checks if nsomething is a digit
+* reset_to_98 - function that change value of a variable pointer is pointing to to 98
 *
-* @c: takes in the integer input
-* Return: 1 if its digit and 0 its not
+* @n: takes in the integer input
 */
 void reset_to_98(int *n);
 {
+	int p;
+
 	*p = &n;
 	p = 98;
 }
