ce8ae49d6fea3874e1f55553ba93c6ffa396613e
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-19 21:41:15 +0200

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,18 +7,18 @@
 * @str: takes in the string
 * Return: 0
 */
-void _puts(char *str)
+void print_rev(char *s)
 {
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; str[i] != 0; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
 		j++;
 	}
-	for (i = i ; str[i] != 0; i--)
+	for (i = i ; s[i] != 0; i--)
 	{
-		printf("%c", str[i]);
+		printf("%c", s[i]);
 	}
 	printf("\n");
 }
