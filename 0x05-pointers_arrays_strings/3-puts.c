905013470e177aae133994a6144f71c5396ed51d
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-19 20:52:00 +0200

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts - change value of a variable pointer is pointing to to 98
+*
+* @str: takes in the string input
+* Return: 0-
+*/
+int _puts(char *str)
+{
+	char me[];
+
+	me = str;
+	printf("%s\n", me);
+	return (0);
+}
