49c71717c8f04e06ae9982743740716b4962db06
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-19 19:18:05 +0200

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* reset_to_98 - function that change value of a variable pointer is pointing to to 98
+* reset_to_98 - change value of a variable pointer is pointing to to 98
 *
 * @n: takes in the integer input
 */
