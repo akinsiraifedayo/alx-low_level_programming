75e436614de4e1920f6be84c72e3d58c16776394
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-19 19:23:01 +0200

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* swap_int - change value of a variable pointer is pointing to to 98
+*
+* @a: takes in the first integer input
+* @b: takes in the second integer input
+*/
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c = *b;
+	*b = *a;
+	*a = *c
+}
