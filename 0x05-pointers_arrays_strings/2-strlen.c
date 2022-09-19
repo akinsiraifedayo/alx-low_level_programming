00c584bc6eb77cac10d1e5edc23acfb332ebf120
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-19 20:26:24 +0200

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strlen - change value of a variable pointer is pointing to to 98
+*
+* @s: takes in the char input
+*/
+int _strlen(char *s)
+{
+	strlen(s);
+}
