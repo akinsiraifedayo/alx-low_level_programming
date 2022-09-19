c0e23a651bf8f9293d11a6f630a0547de5281a1a
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-19 22:05:16 +0200

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* rev_string- prints a string
+*
+* @s: takes in the string
+* Return: 0
+*/
+void rev_string(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+
+	}
+	i--;
+	if (i != 0)
+	{
+		while (i >= 0)
+		{
+			printf("%c", s[i]);
+			i--;
+		}
+	}
+	else
+		printf("\n");
+}
