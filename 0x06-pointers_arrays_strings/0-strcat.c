eabeb13e8c9abf974a4fc0b740dc149ae075c7ac
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-09-21 16:06:38 +0200

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* *_strcat - cocantenate two strings
+*
+* @*dest: first string
+* @*src; second string
+* Description: This is a function to cocantenatre two strings
+*/
+char *_strcat(char *dest, char *src)
+{
+	printf("me");
+}
