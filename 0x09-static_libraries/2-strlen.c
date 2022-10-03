69fe6ec8837a0d27b3778f81fb7bbeb4ab0d962d
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-10-03 04:15:08 -0700

    main.h

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strlen - change value of a variable pointer is pointing to to 98
+*
+* @s: takes in the char input
+* Return: 0
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+
+	}
+	return (i);
+}
