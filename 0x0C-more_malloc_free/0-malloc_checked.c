0bc6aa36aa1117a4f48fe9275f18a340d8a3d3f9
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-10-06 06:38:20 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Creates an array of chars and
+ * @size: The size of the array to be initialized.
+ * @c: The specific char to intialize the array with.
+ *
+ * Return: If size = 0 or malloc fails- NULL else it a pointer to the array.
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = &b;
+
+	if (ptr == 0)
+		return (98);
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		return (98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Ifedayo Akinsira
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0A-malloc_free directory.
+ */
+
+void *malloc_checked(unsigned int b);
+
+#endif
