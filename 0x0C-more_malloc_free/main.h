4d7b4163a03114d5860e427ebd8087c1bc060208
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-10-07 13:40:52 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * File: main.h
- * Auth: Ifedayo Akinsira
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0A-malloc_free directory.
- */
+#ifndef MAINH
+#define MAINH
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+int _putchar(char c);
+
 #endif
