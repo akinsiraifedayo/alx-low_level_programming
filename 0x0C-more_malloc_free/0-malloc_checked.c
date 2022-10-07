ddbb1b673878e17aa388ceb234134c75a3835feb
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-10-07 10:47:01 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 
 /**
- * create_array - Creates an array of chars and
- * @size: The size of the array to be initialized.
- * @c: The specific char to intialize the array with.
+ * malloc_checked - Creates an array of chars and
+ * @b: The specific char to intialize the array with.
  *
- * Return: If size = 0 or malloc fails- NULL else it a pointer to the array.
+ * Return: ptr
  */
 void *malloc_checked(unsigned int b)
 {
